8df784749c38aec82ad1bed50b993ef2d6e24832
jthorat <114556995+jthorat@users.noreply.github.com>
2022-10-04 12:32:48 +0530

    Add files via upload

---
+++ flipbit.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int n,bp;
+printf("enter the n value\n");
+scanf("%d",&n);
+printf("enter the bit position\n");
+scanf("%d",&bp);
+n=n^1<<bp;
+printf("flip bit n:%d\n",n);
+}
