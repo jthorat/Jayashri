56e313316aa406df197f44e4bb2c7d89faea2821
jthorat <114556995+jthorat@users.noreply.github.com>
2022-10-12 17:44:35 +0530

    Update

--- endianness.c
+++ endianness.c
@@ -3,12 +3,12 @@ int main()
 {
 int x=0x01234567;
 char *p=(char*)&x;
-if(*p==0x67)
-{
-printf("little endian\n");
-}
-else
-{
-printf("big endian\n");
-}
+  if(*p==0x67)
+  {
+  printf("little endian\n");
+  }
+  else
+  {
+  printf("big endian\n");
+  }
 }
