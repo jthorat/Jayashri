a02e67f8c2835d7dce324c501388aac538aa5fc4
jthorat <114556995+jthorat@users.noreply.github.com>
2022-10-12 17:29:47 +0530

    Update atoi.c

--- atoi.c
+++ atoi.c
@@ -3,20 +3,21 @@
 int a2i(char*);
 int main(int argc,char* argv[])
 {
-int n;
-n=a2i(argv[1]);
-printf("n=%d\n",n);
+  int n;
+  n=a2i(argv[1]);
+  printf("n=%d\n",n);
 }
+
 int a2i(char* p)
 {
-int i;
-int n=0;
-for(i=0;p[i];i++)
-{
-if(p[i]>='0'&&p[i]<='9')
-n=n*10+(p[i]-48);
-else
-break;
-}
+  int i;
+  int n=0;
+  for(i=0;p[i];i++)
+  {
+    if(p[i]>='0'&&p[i]<='9')
+    n=n*10+(p[i]-48);
+    else
+    break;
+  }
 return n;
 }
