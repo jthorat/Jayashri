5ceedab780673e5319dacf92ea1e72540d9fcd77
jthorat <114556995+jthorat@users.noreply.github.com>
2022-10-12 18:28:24 +0530

    Update primefactor.c

--- primefactor.c
+++ primefactor.c
@@ -1,19 +1,18 @@
 #include<stdio.h>
 int main()
 {
-int n,i=2;
-printf("enter the number\n");
-scanf("%d",&n);
-//for(i=2;n>1;i++)
-while(n>1)
-{
-while(n%i==0)
-{
-printf("%d\n",i);
-n=n/i;
-
-}
-i++;
-}
+  int n,i=2;
+  printf("enter the number\n");
+  scanf("%d",&n);
+  //for(i=2;n>1;i++)
+  while(n>1)
+  {
+    while(n%i==0)
+    {
+      printf("%d\n",i);
+      n=n/i;
+    }
+    i++;
+  }
 }
 
