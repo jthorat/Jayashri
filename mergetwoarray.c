599566ebda600be3c9c49fbb26fad2639cd8f2d8
jthorat <114556995+jthorat@users.noreply.github.com>
2022-10-12 18:10:05 +0530

    Update mergetwoarray.c

--- mergetwoarray.c
+++ mergetwoarray.c
@@ -4,22 +4,23 @@
 char *twostrcat(char* s1,char* s2);
 int main()
 {
-char s1[50],s2[50];
-int i,n;
-printf("enter the string 1\n");
-scanf("%s",s1);
-printf("enter the string 2\n");
-scanf("%s",s2);
-printf("string the s1=%s\n",strcat(s1,s2));
+  char s1[50],s2[50];
+  int i,n;
+  printf("enter the string 1\n");
+  scanf("%s",s1);
+  printf("enter the string 2\n");
+  scanf("%s",s2);
+  printf("string the s1=%s\n",strcat(s1,s2));
 }
+
 char *twostrcat(char* s1,char* s2)
 {
-int i,j;
-for(i=0;s1[i];i++)
-for(j=0;s2[i];j++,i++)
-{
-s1[i]=s2[i];
-}
-s1[i]='\0';
-return s1;
+  int i,j;
+  for(i=0;s1[i];i++)
+  for(j=0;s2[i];j++,i++)
+  {
+    s1[i]=s2[i];
+  }
+  s1[i]='\0';
+  return s1;
 }
