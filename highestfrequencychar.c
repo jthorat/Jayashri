da91062d3b09dd1c32cdce0c75973d22a0139d4d
jthorat <114556995+jthorat@users.noreply.github.com>
2022-10-12 17:52:24 +0530

    Update highestfrequencychar.c

--- highestfrequencychar.c
+++ highestfrequencychar.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 int main()
 {
-int i,count=0;
-char ch;
-printf("enter the charactor\n");
-scanf("%c",&ch);
-char a[10];
-printf("enter the string\n");
-scanf("%s",a);
-for(i=0;a[i];i++)
-{
-if(a[i]==ch)
-count++;
-}
-printf("highest frequency charactor: %d\n",count);
+  int i,count=0;
+  char ch;
+  printf("enter the charactor\n");
+  scanf("%c",&ch);
+  char a[10];
+  printf("enter the string\n");
+  scanf("%s",a);
+  for(i=0;a[i];i++)
+  {
+    if(a[i]==ch)
+    count++;
+  }
+  printf("highest frequency charactor: %d\n",count);
 }
