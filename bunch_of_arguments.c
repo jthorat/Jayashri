f37b73c828adf30d705c183fea98d84346c0ec39
jthorat <114556995+jthorat@users.noreply.github.com>
2022-10-13 14:37:06 +0530

    Update bunch_of_arguments.c

--- bunch_of_arguments.c
+++ bunch_of_arguments.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main(int argc,char const*argv[])
 {
-int i;
-printf("the value of argc is:%d\n",argc);
-for(i=0;i<argc;i++)
-{
-printf("this argument at index no: %d value of %s\n",i,argv[i]);
-}
+  int i;
+  printf("the value of argc is:%d\n",argc);
+  for(i=0;i<argc;i++)
+  {
+    printf("this argument at index no: %d value of %s\n",i,argv[i]);
+  }
 }
