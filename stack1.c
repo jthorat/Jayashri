3a0aabd4ab0c9a91084c043809db054de76d21d1
jthorat <114556995+jthorat@users.noreply.github.com>
2022-10-13 15:03:42 +0530

    Update stack1.c

--- stack1.c
+++ stack1.c
@@ -10,74 +10,80 @@ void display();
 
 int main()
 {
-int choice;
-do
-{
-printf("enter choice 1:push 2:pop 3:peek 4:display 5:exit\n");
-scanf("%d",&choice);
-switch(choice)
-{
-case 1:push();
-break;
-case 2:pop();
-break;
-case 3:peek();
-break;
-case 4:display();
-break;
-case 5:exit(0);
+  int choice;
+  do
+  {
+    printf("enter choice 1:push 2:pop 3:peek 4:display 5:exit\n");
+    scanf("%d",&choice);
+    switch(choice)
+    {
+      case 1:push();
+      break;
+      case 2:pop();
+      break;
+      case 3:peek();
+      break;
+      case 4:display();
+      break;
+      case 5:exit(0);
+    }
+  }
+  while(choice!=0);
 }
-}
-while(choice!=0);
-}
+
+
 void push()
 {
-int x;
-printf("enter data:");
-scanf("%d",&x);
-if(top==N-1)
-{
-printf("overflow\n");
+  int x;
+  printf("enter data:");
+  scanf("%d",&x);
+  if(top==N-1)
+  {
+    printf("overflow\n");
+  }
+  else
+  {
+    top++;
+    stack[top]=x;
+  }
 }
-else
-{
-top++;
-stack[top]=x;
-}
-}
+
 
 void pop()
 {
-int item;
-if(top==-1)
-{
-printf("underflow\n");
+  int item;
+  if(top==-1)
+  {
+    printf("underflow\n");
+  }
+  else
+  {
+    item=stack[top];
+    top--;
+    printf("%d\n",item);
+  }
 }
-else
-{
-item=stack[top];
-top--;
-printf("%d\n",item);
-}
-}
+
 
 void peek()
 {
-if(top==-1)
-{
-printf("underflow\n");
+  if(top==-1)
+  {
+    printf("underflow\n");
+  }
+  else
+  {
+    printf("%d\n",stack[top]);
+  }
 }
-else
-{
-printf("%d\n",stack[top]);
-}
-}
+
+
 void display()
 {
-int i;
-for(i=top;i>=0;i--)
-{
-printf("%d\n",stack[i]);
-}
+  int i;
+  for(i=top;i>=0;i--)
+  {
+    printf("%d\n",stack[i]);
+  }
 }
 
