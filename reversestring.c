9a7bd67fc2ec033094b5a431e668c283e5637293
jthorat <114556995+jthorat@users.noreply.github.com>
2022-10-13 09:33:10 +0530

    Update reversestring.c

--- reversestring.c
+++ reversestring.c
@@ -2,17 +2,17 @@
 #include<string.h>
 int main()
 {
-int i,l;
-char a[10],ch;
-printf("enter the string\n");
-scanf("%s",a);
-printf("before the reverse string:%s\n",a);
-l=strlen(a);
-for(i=0;i<l/2;i++)
-{
-ch=a[i];
-a[i]=a[l-1-i];
-a[l-1-i]=ch;
-}
-printf("after reverse string :%s\n",a);
+  int i,l;
+  char a[10],ch;
+  printf("enter the string\n");
+  scanf("%s",a);
+  printf("before the reverse string:%s\n",a);
+  l=strlen(a);
+  for(i=0;i<l/2;i++)
+  {
+    ch=a[i];
+    a[i]=a[l-1-i];
+    a[l-1-i]=ch;
+  }
+  printf("after reverse string :%s\n",a);
 }
