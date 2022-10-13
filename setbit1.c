b08570fbbb969f0f1b96402f8aec7a47f0db3bc9
jthorat <114556995+jthorat@users.noreply.github.com>
2022-10-13 09:35:28 +0530

    Update setbit1.c

--- setbit1.c
+++ setbit1.c
@@ -1,19 +1,20 @@
 #include<stdio.h>
-void nthbitset(int,int);
+void nth_bit_set(int,int);
 int main()
 {
-int n,bp;
-printf("enter the number\n");
-scanf("%d",&n);
-printf("enter the bit position\n");
-scanf("%d",&bp);
-nthbitset(n,bp);
-return 0;
+  int n,bp;
+  printf("enter the number\n");
+  scanf("%d",&n);
+  printf("enter the bit position\n");
+  scanf("%d",&bp);
+  nth_bit_set(n,bp);
+  return 0;
 }
-void nthbitset(int n,int bp)
+
+void nth_bit_set(int n,int bp)
 {
-if(n>>bp&1)
-printf("set\n");
-else
-printf("not set\n");
+  if(n>>bp&1)
+  printf("set\n");
+  else
+  printf("not set\n");
 }
