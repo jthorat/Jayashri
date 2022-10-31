684d28d3991f17a7b5b0e34cadc54a2c562ddb2d
jthorat <114556995+jthorat@users.noreply.github.com>
2022-10-31 14:55:32 +0530

    Update stack1.c

--- stack1.c
+++ stack1.c
@@ -1,3 +1,5 @@
+/* Implement stack c*/
+
 #include<stdio.h>
 #include<stdlib.h>
 #define N 5
@@ -10,80 +12,80 @@ void display();
 
 int main()
 {
-  int choice;
-  do
-  {
-    printf("enter choice 1:push 2:pop 3:peek 4:display 5:exit\n");
-    scanf("%d",&choice);
-    switch(choice)
+    int choice;
+    do
     {
-      case 1:push();
-      break;
-      case 2:pop();
-      break;
-      case 3:peek();
-      break;
-      case 4:display();
-      break;
-      case 5:exit(0);
+        printf("enter choice 1:push 2:pop 3:peek 4:display 5:exit\n");
+        scanf("%d",&choice);
+        switch(choice)
+        {
+            case 1:push();
+            break;
+            case 2:pop();
+            break;
+            case 3:peek();
+            break;
+            case 4:display();
+            break;
+            case 5:exit(0);
+        }
     }
-  }
-  while(choice!=0);
+    while(choice!=0);
 }
 
 
 void push()
 {
-  int x;
-  printf("enter data:");
-  scanf("%d",&x);
-  if(top==N-1)
-  {
-    printf("overflow\n");
-  }
-  else
-  {
-    top++;
-    stack[top]=x;
-  }
+    int x;
+    printf("enter data:");
+    scanf("%d",&x);
+    if(top==N-1)
+    {
+        printf("overflow\n");
+    }
+    else
+    {
+        top++;
+        stack[top]=x;
+    }
 }
 
 
 void pop()
 {
-  int item;
-  if(top==-1)
-  {
-    printf("underflow\n");
-  }
-  else
-  {
-    item=stack[top];
-    top--;
-    printf("%d\n",item);
-  }
+    int item;
+    if(top==-1)
+    {
+        printf("underflow\n");
+    }
+    else
+    {
+        item=stack[top];
+        top--;
+        printf("%d\n",item);
+    }
 }
 
 
 void peek()
 {
-  if(top==-1)
-  {
-    printf("underflow\n");
-  }
-  else
-  {
-    printf("%d\n",stack[top]);
-  }
+    if(top==-1)
+    {
+        printf("underflow\n");
+    }
+    else
+    {
+        printf("%d\n",stack[top]);
+    }
 }
 
 
 void display()
 {
-  int i;
-  for(i=top;i>=0;i--)
-  {
-    printf("%d\n",stack[i]);
-  }
+    int i;
+    for(i=top;i>=0;i--)
+    {
+        printf("%d\n",stack[i]);
+    }
 }
 
