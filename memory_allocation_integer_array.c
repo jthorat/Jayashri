452f48b6d732d1e12532e3c90ef4ca9682a52a55
jthorat <114556995+jthorat@users.noreply.github.com>
2022-10-31 12:38:02 +0530

    Update memory_allocation_integer_array.c

--- memory_allocation_integer_array.c
+++ memory_allocation_integer_array.c
@@ -1,10 +1,12 @@
+/* How much memory is allocated for a given intiger array? */
+
 #include<stdio.h>
 #include<stdlib.h>
 int main()
 {
-  int n,*p;
-  printf("enter n value\n");
-  scanf("%d",&n);
-  p=(int*)malloc(n*sizeof(int));
-  printf("allocated memory:%d\n",p);
+    int n,*p;
+    printf("enter n value\n");
+    scanf("%d",&n);
+    p=(int*)malloc(n*sizeof(int));
+    printf("allocated memory:%d\n",p);
 }
