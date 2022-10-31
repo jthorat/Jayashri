c61e06350b65c741f133e47f4f8d6fb913173f7e
jthorat <114556995+jthorat@users.noreply.github.com>
2022-10-31 12:03:01 +0530

    Update endianness.c

--- endianness.c
+++ endianness.c
@@ -1,14 +1,16 @@
+/* Write a function to find endianness*/
+
 #include<stdio.h>
 int main()
 {
-int x=0x01234567;
-char *p=(char*)&x;
-  if(*p==0x67)
-  {
-  printf("little endian\n");
-  }
-  else
-  {
-  printf("big endian\n");
-  }
+    int x=0x01234567;
+    char *p=(char*)&x;
+    if(*p==0x67)
+    {
+        printf("little endian\n");
+    }
+    else
+    {
+        printf("big endian\n");
+    }
 }
