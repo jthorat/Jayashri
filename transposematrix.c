8c294d7593e26d29e6faa9b60c685c5772c97774
jthorat <114556995+jthorat@users.noreply.github.com>
2022-10-31 15:06:36 +0530

    Update transposematrix.c

--- transposematrix.c
+++ transposematrix.c
@@ -1,33 +1,35 @@
+/* Write a program to do matrix transpose */
+
 #include<stdio.h>
 int main()
 {
-  int a[2][3],i,j;
-  printf("enter the elements of matrix\n");
-  for(i=0;i<2;i++)
-  {
-    for(j=0;j<3;j++)
+    int a[2][3],i,j;
+    printf("enter the elements of matrix\n");
+    for(i=0;i<2;i++)
     {
-      scanf(" %d",&a[i][j]);
-    }
-  }
+        for(j=0;j<3;j++)
+        {
+            scanf(" %d",&a[i][j]);
+        }
+     }
   
-  printf("matrix is :\n");
-  for(i=0;i<2;i++)
-  {
-    for(j=0;j<3;j++)
-    {
-      printf("%d",a[i][j]);
-    }
-    printf("\n");
-  }
+      printf("matrix is :\n");
+      for(i=0;i<2;i++)
+      {
+          for(j=0;j<3;j++)
+          {
+              printf("%d",a[i][j]);
+          }
+          printf("\n");
+      }
 
-  printf("transpose of matrix :\n");
-  for(i=0;i<3;i++)
-  {
-    for(j=0;j<2;j++)
-    {
-      printf(" %d",a[j][i]);
-    }
-    printf("\n");
-  }
+      printf("transpose of matrix :\n");
+      for(i=0;i<3;i++)
+      {
+          for(j=0;j<2;j++)
+          {
+              printf(" %d",a[j][i]);
+          }
+          printf("\n");
+      }
 }
