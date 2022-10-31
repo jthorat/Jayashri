f12718ba83f59bb34005be7789926ca2cef2db5d
jthorat <114556995+jthorat@users.noreply.github.com>
2022-10-31 12:35:19 +0530

    Update lcmgdc.c

--- lcmgdc.c
+++ lcmgdc.c
@@ -1,24 +1,26 @@
+/* LCM,GCD of 'n' numbers*/
+
 #include<stdio.h>
 int main()
 {
-  int n1,n2,x,y,gcd,lcm;
-  printf("enter the n1 n2 nos\n");
-  scanf("%d%d",&n1,&n2);
-  x=n1;
-  y=n2;
-  while(n1!=n2)
-  {
-    if(n1>n2)
+    int n1,n2,x,y,gcd,lcm;
+    printf("enter the n1 n2 nos\n");
+    scanf("%d%d",&n1,&n2);
+    x=n1;
+    y=n2;
+    while(n1!=n2)
     {
-      n1=n1-n2;
+        if(n1>n2)
+        {
+            n1=n1-n2;
+        }
+        else
+        {
+            n2=n2-n1;
+        }
     }
-    else
-    {
-      n2=n2-n1;
-    }
-  }
-  printf("GCD:%d\n",n1);
-  gcd=n1;
-  lcm=(x*y)/gcd;
-  printf("LCM:%d\n",lcm);
+    printf("GCD:%d\n",n1);
+    gcd=n1;
+    lcm=(x*y)/gcd;
+    printf("LCM:%d\n",lcm);
 }
