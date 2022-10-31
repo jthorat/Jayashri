2d34537dbb7472afb9e611a68a17d8b1060fa7b5
jthorat <114556995+jthorat@users.noreply.github.com>
2022-10-31 16:47:32 +0530

    Update setresetbit.c

--- setresetbit.c
+++ setresetbit.c
@@ -3,24 +3,25 @@
 #include<stdio.h>
 int main()
 {
+    //variable declarition
     int n,bp,op;
-    printf("enter the n value\n");
-    scanf("%d",&n);
-    printf("enter the bit position\n");
-    scanf("%d",&bp);
+    printf("enter the n value\n");              //messg for the user
+    scanf("%d",&n);                             //take the input no from the user
+    printf("enter the bit position\n");         //messg for the user 
+    scanf("%d",&bp);                            // take the bit position input from the user 
     printf("1:set 2:reset\n");
     printf("enter the choice\n");
-    scanf("%d",&op);
-    if(op==1)
+    scanf("%d",&op);                             //select the option from the user
+    if(op==1)                                    //compare the option                                  
     {
-        n=n|(1<<bp);
+        n=n|(1<<bp);                             //logic of the set bit 
         printf("set bit n:%d\n",n);
     }
-    else if(op==2)
+    else if(op==2)                              //compare the option
     {
-        n=n&~(1<<bp);
+        n=n&~(1<<bp);                           //logic of the clear bit
         printf("reset bit n:%d\n",n);
     }
     else
-    printf("invalid option\n");
+    printf("invalid option\n");                 //default  option
 }
