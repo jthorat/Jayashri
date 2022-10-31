000a42c7920ff6c7880edd4679449b0f5d61339b
jthorat <114556995+jthorat@users.noreply.github.com>
2022-10-31 16:57:10 +0530

    Update setbit1.c

--- setbit1.c
+++ setbit1.c
@@ -1,21 +1,22 @@
 /* Write a function to return whether n-th bit from right to left is set in an integer. */
 
 #include<stdio.h>
-void nth_bit_set(int,int);
+void nth_bit_set(int,int);                //function declarition
 int main()
 {
+    //variable declarition
     int n,bp;
-    printf("enter the number\n");
-    scanf("%d",&n);
-    printf("enter the bit position\n");
-    scanf("%d",&bp);
-    nth_bit_set(n,bp);
+    printf("enter the number\n");        //messg for the user
+    scanf("%d",&n);                      //take the input from the user
+    printf("enter the bit position\n");  //messg for the user
+    scanf("%d",&bp);                     //take the bit position input from the user
+    nth_bit_set(n,bp);                   //function calling and passing n and bp as the actual parameters
     return 0;
 }
 
-void nth_bit_set(int n,int bp)
+void nth_bit_set(int n,int bp)          // function defination
 {
-    if(n>>bp&1)
+    if(n>>bp&1)                         //logic of the set bit
         printf("set\n");
     else
         printf("not set\n");
