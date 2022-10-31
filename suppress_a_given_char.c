fc75f38677e7debc3375b11420a73c02f3b51b28
jthorat <114556995+jthorat@users.noreply.github.com>
2022-10-31 16:05:19 +0530

    Update suppress_a_given_char.c

--- suppress_a_given_char.c
+++ suppress_a_given_char.c
@@ -2,35 +2,37 @@
 
 #include<stdio.h>
 #include<string.h>
-void suppress_a_given_charactor(char*,char);
+void suppress_a_given_charactor(char*,char);     //function declaration
 
 int main()
 {
+   //variable declaration
     char ch;
     printf("enter the charactor\n");
-    scanf("%c",&ch);
+    scanf("%c",&ch);                            //take input from user
     char s[50];
     printf("enter the string\n");
-    scanf("%s",s);
-    suppress_a_given_charactor(s,ch);
+    scanf("%s",s);                              //take string input form user
+    suppress_a_given_charactor(s,ch);           //calling function and passing s and ch as actual parameters
     printf("the final string '%c'=%s\n",ch,s);
     return 0;
 }
 
 
-void suppress_a_given_charactor(char* s,char ch)
+void suppress_a_given_charactor(char* s,char ch)//func defination with s and ch as FP
 {
+    //varible dec
     int l,i,j;
-    l=strlen(s);
+    l=strlen(s);                                //calling fun string len and storing result into l
     for(i=0;i<l;i++)
     {
-        if(s[i]==ch)
+        if(s[i]==ch)                            //if current string char is compared with given char if matched true
         {
-            for(j=i;j<l;j++)
-            {
+            for(j=i;j<l;j++)                    
+            {                                   //logic to delete char from string
                 s[j]=s[j+1];
             }
-            l--;
+            l--;                                //dec len and loop counter
             i--;
         }
     }
