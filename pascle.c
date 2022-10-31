d5ae73c34893c82604d85dfe7527fdec86e58699
jthorat <114556995+jthorat@users.noreply.github.com>
2022-10-31 12:56:12 +0530

    Update pascle.c

--- pascle.c
+++ pascle.c
@@ -1,21 +1,25 @@
+/* Print pacsle triangle */
+
 #include<stdio.h>
 int main()
 {
-  int row,col,space,n,no;
-  printf("enter the no of rows\n");
-  scanf("%d",&no);
-  for(row=0;row<no;row++)
-  {
-    for(space=0;space<(no-row);space++)
+    int row,col,space,n,no;
+    printf("enter the no of rows\n");
+    scanf("%d",&no);
+    for(row=0;row<no;row++)
     {
-      printf(" ");
+        for(space=0;space<(no-row);space++)
+        {
+            printf(" ");
+        }
+      
+        n=1;
+      
+        for(col=0;col<=row;col++)
+        {
+            printf(" %d",n);
+            n=n*(row-col)/(col+1);
+        }
+        printf("\n");
     }
-    n=1;
-    for(col=0;col<=row;col++)
-    {
-      printf(" %d",n);
-      n=n*(row-col)/(col+1);
-    }
-    printf("\n");
-  }
 }
