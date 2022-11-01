fd7597fce0822ec2d83f0137dc68d2d282012f72
jthorat <114556995+jthorat@users.noreply.github.com>
2022-11-01 13:13:00 +0530

    Update endianness.c

--- endianness.c
+++ endianness.c
@@ -3,14 +3,15 @@
 #include<stdio.h>
 int main()
 {
-    int x=0x01234567;
-    char *p=(char*)&x;
-    if(*p==0x67)
+    //variable declarition
+    int x=0x01234567;          
+    char *p=(char*)&x;               //stored the address of x variable in pointer and typecasted to the int variable
+    if(*p==0x67)                     
     {
-        printf("little endian\n");
+        printf("little endian\n");   //little endian architecture you will get 0x67 
     }
     else
     {
-        printf("big endian\n");
+        printf("big endian\n");      // big endian architecture you will get 0x01
     }
 }
