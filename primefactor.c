b1e611031312d0663c0c13b5317db0b2ca640c81
jthorat <114556995+jthorat@users.noreply.github.com>
2022-11-01 10:44:22 +0530

    Update primefactor.c

--- primefactor.c
+++ primefactor.c
@@ -3,14 +3,15 @@
 #include<stdio.h>
 int main()
 {
+    // variable declarition
     int n,i=2;
-    printf("enter the number\n");
-    scanf("%d",&n);
+    printf("enter the number\n");  //messg for user
+    scanf("%d",&n);                // take input from the user   
     while(n>1)
     {
-        while(n%i==0)
+        while(n%i==0)             // logic of the finding prime factorof given number
         {
-            printf("%d\n",i);
+            printf("%d\n",i);     // print the output   
             n=n/i;
         }
         i++;
