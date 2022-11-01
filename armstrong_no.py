4ae7e5c0c65828dd522dd7b09bd401e066666260
jthorat <114556995+jthorat@users.noreply.github.com>
2022-11-01 15:13:53 +0530

    Update armstrong_no.py

--- armstrong_no.py
+++ armstrong_no.py
@@ -1,11 +1,12 @@
-Num=int(input("Enter The Number"))
+# Armstrong number
+Num=int(input("Enter The Number"))    # take the input from the user
 temp=Num
 Sum=0
-while(Num>0):
+while(Num>0):                         # logic of the finding armstrong number 
     Rem=Num%10
-    Sum=Sum+Rem**3
+    Sum=Sum+Rem**3                    
     Num=Num//10
-if (temp==Sum):
-    print("The Armstrong Number")
+if (temp==Sum):                       # user input and the after logic get output is equal condition true
+    print("The Armstrong Number")     # messg for user
 else:
-    print("Not Armstrong Number")
+    print("Not Armstrong Number")     # messg for user
