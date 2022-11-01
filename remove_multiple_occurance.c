13c27a6b16181c1601dc3e67584f229df7bdcf59
jthorat <114556995+jthorat@users.noreply.github.com>
2022-11-01 10:40:04 +0530

    Update remove_multiple_occurance.c

--- remove_multiple_occurance.c
+++ remove_multiple_occurance.c
@@ -3,25 +3,26 @@
 
 int main()
 {
+	// variable declarition
 	int len,i,j;
 	char ch;
-	printf("enter the charactor:");
-	scanf("%c",&ch);
-	char a[50];
-	printf("enter the string:");
-	scanf("%s",a);
-	len=strlen(a);
+	printf("enter the charactor:");                                 //messg for user
+	scanf("%c",&ch);                                                //take input from the user 
+	char a[50];    
+	printf("enter the string:");                                    //messg for the user
+	scanf("%s",a);                                                  // take the string from the user 
+	len=strlen(a);                                                  //count the length of the string and stored in len variable
 	for(i=0;a[i];i++)
 	{
-		if(a[i]==ch)
+		if(a[i]==ch)                                            //compare the string charactors with user given charactor
 		{
 			for(j=i;j<len;j++)
 			{
-				a[j]=a[j+1];
+				a[j]=a[j+1];                           //if cond true then skip these char and move next address
 			}
-			len--;
+			len--;                                         // string length is decrement
 			i--;
 		}
 	}
-	printf("\nThe final string removing all occurance : %s\n",a);
+	printf("\nThe final string removing all occurance : %s\n",a);   // print the output remove the multiple occurance
 } 
