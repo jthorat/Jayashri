0c07f53c39d8d34e79e6cf7ead9ce66525205d68
jthorat <114556995+jthorat@users.noreply.github.com>
2022-11-01 11:15:04 +0530

    Update minmax.c

--- minmax.c
+++ minmax.c
@@ -3,20 +3,21 @@
 #include<stdio.h>
 int main()
 {
+    // variable declarition
     int a[5]={2,6,7,9,5};
     int n,max,min,i;
     n=sizeof(a)/sizeof(a[0]);
-    max=a[0];
-    min=a[0];
+    max=a[0];                                         // array index[0]th value is stored in max variable
+    min=a[0];                                         // array index[0]th value is stored in min variable  
     for(i=0;i<n;i++)
     {
-        if(a[i]<min)
+        if(a[i]<min)                                  // logic of the finding min number 
         min=a[i];
     
-        if(a[i]>max)
+        if(a[i]>max)                                 //logic of the finding max number
         max=a[i];
     }
-    printf("largest element in the array:%d\n",max);
-    printf("smallest element in the array:%d\n",min);
+    printf("largest element in the array:%d\n",max); //print the max number 
+    printf("smallest element in the array:%d\n",min);//print the min number
 }
 
