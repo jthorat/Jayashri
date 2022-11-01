426bef17949e0c4ece72d8b5e1bb74d0e212c4ce
jthorat <114556995+jthorat@users.noreply.github.com>
2022-11-01 15:07:01 +0530

    Update atoi.c

--- atoi.c
+++ atoi.c
@@ -1,26 +1,28 @@
 /* Write a function to convert string to integer,equivalent of atoi().*/
 
-#include<stdio.h>
+#include<stdio.h>             
 #include<stdlib.h>
-int a2i(char*);
-int main(int argc,char* argv[])
+int a2i(char*);                                      //function declarition
+int main(int argc,char* argv[])                      // command line argument
 {
+    // variable declarition
     int n;
-    n=a2i(argv[1]);
-    printf("n=%d\n",n);
+    n=a2i(argv[1]);                                  //atoi function calling 
+    printf("n=%d\n",n);                              // print the output
 }
  
 
-int a2i(char* p)
+int a2i(char* p)                                     //function defination
 {
+    //variable declarition
     int i;
     int n=0;
     for(i=0;p[i];i++)
     {
         if(p[i]>='0'&&p[i]<='9')
-        n=n*10+(p[i]-48);
+        n=n*10+(p[i]-48);                          // logic of the atoi  
         else  
         break;
     }
-    return n;
+    return n;                                     // return to the n variable 
 }
