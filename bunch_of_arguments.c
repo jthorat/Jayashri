060c661c5cd5acb3502aa754231eb9cd638fea4b
jthorat <114556995+jthorat@users.noreply.github.com>
2022-11-01 14:42:37 +0530

    Update bunch_of_arguments.c

--- bunch_of_arguments.c
+++ bunch_of_arguments.c
@@ -1,7 +1,8 @@
 /* Process bunch of inputs arguments and print them*/
 #include<stdio.h>
-int main(int argc,char const*argv[])
+int main(int argc,char const*argv[])                                    
 {
+    // variable declarition
     int i;
     printf("the value of argc is:%d\n",argc);
     for(i=0;i<argc;i++)
