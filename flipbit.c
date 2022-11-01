e3a2954f5c9fb90cbe140b1a1d1650c686b812e5
jthorat <114556995+jthorat@users.noreply.github.com>
2022-11-01 11:46:48 +0530

    Update flipbit.c

--- flipbit.c
+++ flipbit.c
@@ -3,11 +3,12 @@
 #include<stdio.h>
 int main()
 {
+    //variable declarition
     int n,bp;
-    printf("enter the n value\n");
-    scanf("%d",&n);
-    printf("enter the bit position\n");
-    scanf("%d",&bp);
-    n=n^1<<bp;
-    printf("flip bit n:%d\n",n);
+    printf("enter the n value\n");      // messg for the user
+    scanf("%d",&n);                     //take input from the user
+    printf("enter the bit position\n"); //messg for the user
+    scanf("%d",&bp);                    // take input from the user
+    n=n^1<<bp;                          // logic of the flip bit  
+    printf("flip bit n:%d\n",n);        // print the output
 }
