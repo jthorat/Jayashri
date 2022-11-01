cf4aec8382d03024af7eb785e596d96a08a3bbb2
jthorat <114556995+jthorat@users.noreply.github.com>
2022-11-01 12:59:57 +0530

    Update highestfrequencychar.c

--- highestfrequencychar.c
+++ highestfrequencychar.c
@@ -3,17 +3,18 @@
 #include<stdio.h>
 int main()
 {
+    //variable declaration
     int i,count=0;
     char ch;
-    printf("enter the charactor\n");
-    scanf("%c",&ch);
+    printf("enter the charactor\n");                   // messg for the user
+    scanf("%c",&ch);                                   //  take the input from the user
     char a[10];
-    printf("enter the string\n");
-    scanf("%s",a);
+    printf("enter the string\n");                      // messg for the user 
+    scanf("%s",a);                                     // take the input from the user
     for(i=0;a[i];i++)
     {
-        if(a[i]==ch)
-        count++;
+        if(a[i]==ch)                                   // compare the user given char in the string
+        count++;                                       //count user given char how many times and stored in count variable
     }
-    printf("highest frequency charactor: %d\n",count);
+    printf("highest frequency charactor: %d\n",count); // print the output
 }
