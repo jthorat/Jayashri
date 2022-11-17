65d02af21f9b7d5b1f30cdcca5adacde8f15b13f
jthorat <114556995+jthorat@users.noreply.github.com>
2022-11-17 13:27:42 +0530

    Update bunch_of_arguments.c

--- bunch_of_arguments.c
+++ bunch_of_arguments.c
@@ -1,13 +1,18 @@
 /* Process bunch of inputs arguments and print them*/
-/* command line arguments */
+
 #include<stdio.h>
-int main(int argc,char const*argv[])                                       // argc-counts the no of arguments,argv-contain the total no of argument                                
+
+// command line arguments 
+// argc-counts the no of arguments,argv-contain the total no of argument
+int main(int argc,char const*argv[])                                                                      
 {
     // variable declarition
     int i;
     printf("the value of argc is:%d\n",argc);
     for(i=0;i<argc;i++)
     {
-        printf("this argument at index no: %d value of %s\n",i,argv[i]); // print the output
+        // print the output
+        printf("this argument at index no: %d value of %s\n",i,argv[i]); 
     }
+    
 }
