04dd75668aaea5bcd43124817523c8de9061f9b3
jthorat <114556995+jthorat@users.noreply.github.com>
2022-12-16 10:59:24 +0530

    Update getchar.c

--- getchar.c
+++ getchar.c
@@ -5,16 +5,19 @@
 #include <fcntl.h>
 #include<errno.h>
 #include<stdlib.h>
-
+//command line argument
 int main(int argc,char *argv[])
 {
+	//variable declarition
 	int fd;
 	char ch;
+	//file open in read mode only
 	fd=open(argv[1],O_RDONLY);
 	if(fd<0)
 	{
 		perror(" file cannot open");
 	}
+	//read the char in the file
 	read(1,&ch,1);
 	printf("%c",ch);
 	
