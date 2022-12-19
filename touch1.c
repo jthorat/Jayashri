72e83963bc2bbffe6493a85bc2723b1ef7d1941d
jthorat <114556995+jthorat@users.noreply.github.com>
2022-12-19 10:34:10 +0530

    Update touch1.c

--- touch1.c
+++ touch1.c
@@ -1,8 +1,12 @@
+// touch command
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
 #include<sys/stat.h>
 #include<fcntl.h>
+
+//command line argument
 int main(int argc,char *argv[])
 {
 	if(argc != 2)
@@ -10,8 +14,12 @@ int main(int argc,char *argv[])
 		printf("./a.out filename");
 		return 0;
 	}
+	
+	//variable declarition
 	int in;
+	//open file in read mode and write mode by using user
 	in=open(argv[1],O_RDWR|O_CREAT,S_IRUSR|S_IWUSR);
+	//close file
 	close(in);
 	return 0;
 }
